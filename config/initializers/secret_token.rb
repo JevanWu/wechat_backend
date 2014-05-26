06134e9637795c6b8f66e47bf715aa1839738d7f
jevanwu <jevanwu@gmail.com>
2014-05-26 16:29:28 +0800

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HualiWechat::Application.config.secret_key_base = '04ad896045ca19e26c2c4e13055021867b13cf882b6a0f57ec03bd4edf9e69234345d8d638ef67d21b109f3ac892072fe0257fa8e159648972313b8866ad3157'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HualiWechat::Application.config.session_store :cookie_store, key: '_huali_wechat_session'
