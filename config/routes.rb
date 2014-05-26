4431aac9b9841c9c53114e765a53da6ce56bff32
jevanwu <jevanwu@gmail.com>
2014-05-26 18:36:45 +0800

    [feature] create auto-replies

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ HualiWechat::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'pages#index'
+  root 'replies#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
