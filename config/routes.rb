41ad5fe12fe3b5ef08fe32898dd7d4541346df73
jevanwu <jevanwu@gmail.com>
2014-06-04 18:04:36 +0800

    modify the root of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ HualiWechat::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'replies#index'
+  root 'auto_replies#index'
   resources :assets
   resources :image_texts
   resources :image_text_collections
