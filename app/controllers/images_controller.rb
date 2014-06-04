37b0dec39878d24668f311d87018acf525d0e230
jevanwu <jevanwu@gmail.com>
2014-06-04 17:01:19 +0800

    add 'destroy' and 'edit' for images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -13,8 +13,27 @@ class ImagesController < ApplicationController
     redirect_to image_path(@image)
   end
 
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    if @image.update(image_params)
+      redirect_to image_path(@image)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @image = Image.find(params[:id])
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to images_path
   end
 
   private
