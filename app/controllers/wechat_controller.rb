b7397ad8e07e8b22fa2720c5043a3b625e7a1018
jevanwu <jevanwu@gmail.com>
2014-06-09 11:07:47 +0800

    check signature sent from wechat server

--- app/controllers/wechat_controller.rb
+++ app/controllers/wechat_controller.rb
@@ -1,6 +1,34 @@
+require 'digest/sha1'
 class WechatController < ApplicationController
   respond_to :json
   
   def receiver
+    signature = params[:signature]
+    timestamp = params[:timestamp]
+    nonce = params[:nonce]
+    token = "EuK8378d11ckeRXam86T5Pt37z6W1F8b"
+
+    if check_signature(signature, timestamp, nonce)
+      render text: nonce
+    end
+  end
+
+  private
+
+  def check_signature(signature, timestamp, nonce)
+    decrypted_token = decrypt(signature, timestamp, nonce)
+
+    if decrypted_token == token
+      return true
+    else
+      return false
+    end
+  end
+
+  def decrypt(signature, timestamp, nonce)
+    credential = [signature, timestamp, nonce]
+    credential.sort
+    credential.join("")
+    Digest::SHA1.hexdigest credential
   end
 end
