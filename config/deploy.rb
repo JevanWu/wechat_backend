ac6f40e05de11f2be3987289ee0f648ee27a8589
jevanwu <jevanwu@gmail.com>
2014-06-11 16:09:57 +0800

    ignore application.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '~/repositories/huali_wechat'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/unicorn.rb}
+set :linked_files, %w{config/database.yml config/unicorn.rb config/application.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
