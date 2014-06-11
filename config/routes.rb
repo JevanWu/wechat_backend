b16a00facee35be9661dea967a2407d3498b77bd
jevanwu <jevanwu@gmail.com>
2014-06-11 15:57:10 +0800

    create views and action for 'default_reply'

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ HualiWechat::Application.routes.draw do
 
   get 'subscribe_reply', to: 'auto_replies#subscribe_reply', as: 'subscribe_reply'
   patch 'subscribe_reply/:id', to: 'auto_replies#update_subscribe_reply', as: 'update_subscribe_reply'
+  get 'default_reply', to: 'auto_replies#default_reply', as: 'default_reply'
+  patch 'default_reply/:id', to: 'auto_replies#update_default_reply', as: 'update_default_reply'
   get 'wechat/receiver', to: 'wechat#receiver'
   post 'wechat/receiver', to: 'wechat#receiver'
   # You can have the root of your site routed with "root"
