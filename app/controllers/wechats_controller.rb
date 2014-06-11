4cb63641eb9279f37fea6ccc4fd6d19c3d052f5b
jevanwu <jevanwu@gmail.com>
2014-06-11 17:58:14 +0800

    use text message for test

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -2,8 +2,13 @@ class WechatsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:receiver]
   wechat_responder
 
-  on :event, with "subscribe" do |request|
-    message = TextMessage.where(label: "subscribe").first
-    request.reply.text "#{message.content}"
+  # on :event, with "subscribe" do |request|
+  #   message = TextMessage.where(label: "subscribe").first
+  #   request.reply.text "#{message.content}"
+  # end
+
+  on :text do |request, content|
+    message = TextMessage.where(label: "default").first
+    request.reply.text "echo: #{message.content}"
   end
 end
