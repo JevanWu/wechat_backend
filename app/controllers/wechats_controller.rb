1ce06872648cbe04b71137df34e97c51f4da3e3e
jevanwu <jevanwu@gmail.com>
2014-06-12 15:19:51 +0800

    add event replies and key word reply

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,13 +1,22 @@
 class WechatsController < ApplicationController
   wechat_responder
 
-  # on :event, with "subscribe" do |request|
-  #   message = TextMessage.where(label: "subscribe").first
-  #   request.reply.text "#{message.content}"
-  # end
+  on :event, with: "Subscribe" do |request|
+    message = TextMessage.where(label: "subscribe").first
+    request.reply.text "#{message.content}" unless message.nil?
+  end
+
+  on :event, with: "Unsubscribe" do |request|
+    message = TextMessage.where(label: "unsubscribe").first
+    request.reply.text "#{message.content}" unless message.nil?
+  end
 
   on :text do |request, content|
     message = TextMessage.where(label: "default").first
     request.reply.text message.content unless message.nil?
+  end
+
+  on :text, with: "help" do |request, help|
+    request.reply.text "This is help content"
   end
 end
