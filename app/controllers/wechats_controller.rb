a65094c104871801bb751b97b512d78bb8621886
jevanwu <jevanwu@gmail.com>
2014-06-23 12:34:13 +0800

    modify we chats controller to avoid conflicts

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,37 +1,37 @@
 class WechatsController < ApplicationController
   wechat_responder
 
-  on :event, with: "subscribe" do |request|
-    reply = SubscribeReply.first
-    request.reply.text reply.content unless reply.nil?
-  end
-
-  on :text do |request, content|
-    reply = DefaultReply.first
-    request.reply.text reply.content unless reply.nil?
-  end
-
   #key words replies
-  OrdinaryReply.all.each do |reply|
-    reply.keywords.each do |keyword|
+  OrdinaryReply.all.each do |keyword_reply|
+    keyword_reply.keywords.each do |keyword|
       on :text, with: keyword.keyword do |request|
 
-        request.reply.text reply.content if reply.asset_id.nil?
+        request.reply.text keyword_reply.content if keyword_reply.asset_id.nil?
 
-        if reply.asset.is_a?NewsAssetCollection
+        if keyword_reply.asset.is_a?NewsAssetCollection
           logger.info "news asset collection"
-          collection = reply.asset.news_assets
+          collection = keyword_reply.asset.news_assets
           assets_count = collection.count
           articles_range = (0... [assets_count, 10].min)
           request.reply.news(articles_range) do |article, i| 
             article.item title: collection[i].title, description: collection[i].description, pic_url: "http://wechat.hua.li" + collection[i].cover.url, url: collection[i].url
           end
           
-        elsif reply.asset.is_a?ImageAsset 
-          logger.info "media_id:" + reply.asset.media_id.inspect
-          request.reply.image reply.asset.media_id
+        elsif keyword_reply.asset.is_a?ImageAsset 
+          logger.info "media_id:" + keyword_reply.asset.media_id.inspect
+          request.reply.image keyword_reply.asset.media_id
         end
       end
     end
+  end
+
+  on :text do |request, content|
+    default_reply = DefaultReply.first
+    request.reply.text default_reply.content unless default_reply.nil?
+  end
+  
+  on :event, with: "subscribe" do |request|
+    subscribe_reply = SubscribeReply.first
+    request.reply.text subscribe_reply.content unless subscribe_reply.nil?
   end
 end
