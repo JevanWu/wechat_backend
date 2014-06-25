e8efd6caadb1435adb5c8e08c77bdf13300d39db
jevanwu <jevanwu@gmail.com>
2014-06-25 18:08:22 +0800

    improve the nested form of news asset collections

--- app/assets/javascripts/image_text_collections.js
+++ app/assets/javascripts/image_text_collections.js
@@ -4,10 +4,10 @@ function remove_fields(link) {
   $(link).closest(".fields").hide();
 }
 
-function add_fields(link, trigger, field) {
+function add_fields(link, collection, field) {
   var new_id = new Date().getTime();
-  var trigger_regexp = new RegExp("new_trigger", "g");
+  var collection_regexp = new RegExp("new_collection", "g");
   var field_regexp = new RegExp("new_field", "g");
-  $(link).before(trigger.replace(trigger_regexp, new_id)); 
+  $(link).before(collection.replace(collection_regexp, new_id)); 
   $("input[type=submit]").before(field.replace(field_regexp, new_id));
 }
