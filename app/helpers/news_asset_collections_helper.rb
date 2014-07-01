4695f4275f1e7462a2f15dbc845cb6a38d637c90
jevanwu <jevanwu@gmail.com>
2014-07-01 16:13:38 +0800

    use image_path helper to show the right image path

--- app/helpers/news_asset_collections_helper.rb
+++ app/helpers/news_asset_collections_helper.rb
@@ -5,7 +5,7 @@ module NewsAssetCollectionsHelper
 
   def link_to_add_field(name, f)
     new_obj = f.object.class.reflect_on_association(:news_assets).klass.new
-    collection = "<div class=\"thumbnail fields\"><img alt=\"Default image\" data-target=\"#modalnew_collection\" data-toggle=\"modal\" src=\"/assets/default_image.gif\"><a class=\"btn btn-default\" href=\"#\" onclick=\"NewsAsset.remove_fields(this); return false;\">Remove</a></div>"
+    collection = "<div class=\"thumbnail fields\"><img alt=\"Default image\" data-target=\"#modalnew_collection\" data-toggle=\"modal\" src=\"#{image_path "default_image.gif"}\"><a class=\"btn btn-default\" href=\"#\" onclick=\"NewsAsset.remove_fields(this); return false;\">Remove</a></div>"
     field = f.simple_fields_for :news_assets, new_obj ,child_index: "new_field" do |sf|
       render "news_asset_field", f: sf, index: "new_field"
     end
