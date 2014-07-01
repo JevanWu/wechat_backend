1d146658dfda263d3f9ee69d1422baa8cb446dcb
jevanwu <jevanwu@gmail.com>
2014-07-01 18:19:18 +0800

    merge "image" and "news" to one input

--- app/helpers/replies_helper.rb
+++ app/helpers/replies_helper.rb
@@ -3,10 +3,10 @@ module RepliesHelper
     case type
     when "text"
       builder.input :content, input_html: { class: "summernote" }
-    when "image"
-      builder.input :asset_id, as: :select, collection: ImageAsset.ids
-    when "news"
-      builder.input :asset_id, as: :select, collection: NewsAsset.ids
+    when "asset"
+      builder.input :asset_id, as: :select, collection: Asset.ids
+    # when "news"
+    #   builder.input :asset_id, as: :select, collection: NewsAsset.ids
     end
   end
 end
