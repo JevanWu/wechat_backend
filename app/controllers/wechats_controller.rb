0b626b5c01b6aaf8072e2b02282313252fe301ca
jevanwu <jevanwu@gmail.com>
2014-07-03 16:44:50 +0800

    rebuild the responder of keyword replies right before create action

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,29 +1,6 @@
 class WechatsController < ApplicationController
   wechat_responder
-
-  #key words replies
-  def create
-    @keyword = Keyword.find_by(keyword: params[:xml][:Content]) if params[:xml][:MsgType] == "text"
-    super
-  end
-
-  if !@keyword.nil?
-    on :text, with: @keyword.keyword do |request| 
-      ordinary_reply = @keyword.ordinary_reply
-      if ordinary_reply.asset_id.nil?
-        request.reply.text ordinary_reply.content 
-      elsif ordinary_reply.asset.is_a?NewsAssetCollection
-        news_collection = ordinary_reply.asset.news_assets
-        assets_count = news_collection.count
-        articles_range = (0... [assets_count, 10].min)
-        request.reply.news(articles_range) do |article, i| 
-          article.item title: news_collection[i].title, description: news_collection[i].description, pic_url: ENV["DOMAIN_NAME"] + news_collection[i].cover.url, url: news_collection[i].url
-        end
-      elsif ordinary_reply.asset.is_a?ImageAsset 
-        request.reply.image ordinary_reply.asset.media_id
-      end
-    end
-  end
+  brefore_action :build_keywords_replies, only: [:create]
 
   on :text do |request, content|
     default_reply = DefaultReply.first
@@ -33,5 +10,28 @@ class WechatsController < ApplicationController
   on :event, with: "subscribe" do |request|
     subscribe_reply = SubscribeReply.first
     request.reply.text subscribe_reply.content unless subscribe_reply.nil?
+  end
+
+  private
+
+  def build_keywords_replies 
+    #key words replies
+    OrdinaryReply.all.each do |ordinary_reply|
+      ordinary_reply.keywords.each do |keyword|
+        on :text, with: keyword.keyword do |request|
+          request.reply.text ordinary_reply.content if ordinary_reply.asset_id.nil?
+          if ordinary_reply.asset.is_a?NewsAssetCollection
+            news_collection = ordinary_reply.asset.news_assets
+            assets_count = news_collection.count
+            articles_range = (0... [assets_count, 10].min)
+            request.reply.news(articles_range) do |article, i| 
+              article.item title: news_collection[i].title, description: news_collection[i].description, pic_url: ENV["DOMAIN"] + news_collection[i].cover.url, url: news_collection[i].url
+            end
+          elsif ordinary_reply.asset.is_a?ImageAsset 
+            request.reply.image ordinary_reply.asset.media_id
+          end
+        end
+      end
+    end
   end
 end
