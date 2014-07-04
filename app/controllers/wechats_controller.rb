c689402b5056cd878494f3eda0200bd0fcacaa0d
jevanwu <jevanwu@gmail.com>
2014-07-04 17:32:22 +0800

    change the name of global variable

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -25,7 +25,7 @@ class WechatsController < ApplicationController
             assets_count = news_collection.count
             articles_range = (0... [assets_count, 10].min)
             request.reply.news(articles_range) do |article, i| 
-              article.item title: news_collection[i].title, description: news_collection[i].description, pic_url: ENV["DOMAIN"] + news_collection[i].cover.url, url: news_collection[i].url
+              article.item title: news_collection[i].title, description: news_collection[i].description, pic_url: ENV["DOMAIN_NAME"] + news_collection[i].cover.url, url: news_collection[i].url
             end
           elsif keyword_reply.asset.is_a?ImageAsset 
             if keyword_reply.asset.updated_at.to_date <= 3.days.from_now
